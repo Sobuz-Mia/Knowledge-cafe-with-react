c443935f57c48b77ea4fea5d2f1f4bf44d2ade8f
Sobuz <sobuz.info0298@gmail.com>
2023-09-12 13:53:44 +0600

    header complete

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -2,7 +2,7 @@ import profile from '../../assets/images/profile.png'
 const Header = () => {
     return (
         <header>
-            <div>
+            <div className='flex justify-between p-4 mx-4 border-b-2'>
                 <h1 className="text-5xl font-bold">Knowledge cafe</h1>
                 <img src={profile} alt="" />
             </div>
